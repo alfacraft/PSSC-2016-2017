edb43e62a207eac9b3ca9fbb1ebaf31be15dea44
RaduCraciunescu <raducraciunescu@yahoo.com>
2017-01-19 20:30:22 +0200

    Radu Craciunescu Final

---
+++ PSSC/Models/EventHandler/IEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using CQRS.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.EventHandler
+{
+    public interface IEventHandler<TEvent> where TEvent : Event
+    {
+        void Handle(TEvent handle);
+    }
+}
